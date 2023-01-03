e07f35e39c76bbc110050ab3546ca6c76b2e0abf
Angelo Antonio Salatino <angelosalatino@users.noreply.github.com>
2023-01-03 11:16:08 +0000

    prepared some tests

---
+++ test.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  3 10:37:17 2023
+
+@author: aas358
+"""
+
+from dblp_parser import DBLP
+
+
+
+def parse_year_2022():
+    dblp = DBLP()
+    
+    dblp_path = "dblp.xml"
+    save_path = "dblp.json"
+    dblp.parse_all("2022", dblp_path, save_path)
+    
+    
+def parse_everything():
+    dblp = DBLP()
+    
+    dblp_path = "dblp.xml"
+    save_path = "dblp.json"
+    dblp.parse_all(dblp_path, save_path)
+    
+    
+def parse_a_selectio_of_features():
+    dblp = DBLP()
+    
+    dblp_path = "dblp.xml"
+    save_path = "dblp.jsonl"
+    features = {"url", "author", "ee", "journal", "number", "pages", "publisher", "series","booktitle", "title", "volume", "year"}
+    dblp.parse_all(dblp_path, save_path, features_to_extract=features)
+    
+def generate_dataframe():
+    dblp = DBLP()
+    
+    dblp_path = "dblp.xml"
+    features = {"url", "author", "ee", "journal", "number", "pages", "publisher", "series","booktitle", "title", "volume", "year"}
+    df = dblp.parse_all(dblp_path, features_to_extract=features, output="dataframe")
+    print(df)
+
+
+def main():
+    """
+    Main function
+
+    """
+    
+    # parse_everything()
+    # parse_a_selectio_of_features()
+    # generate_dataframe()
+    parse_year_2022()
+
+
+if __name__ == '__main__':
+    main()
